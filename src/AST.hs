9b32c0edb162d5d7093694bc665d46af98c7336c
quasarbright <mthundershock@gmail.com>
2021-01-25 19:41:35 -0500

    finish and add ints

--- src/AST.hs
+++ src/AST.hs
@@ -5,20 +5,23 @@ import Data.Map(Map)
 import qualified Data.Map as Map
 import Data.Set(Set)
 import qualified Data.Set as Set
+import Data.Maybe (fromMaybe)
   
 data Expr = Var String
           | Lambda String Expr
           | App Expr Expr
           | Let String Expr Expr
-          deriving(Eq, Ord)
+          | Int Integer
+          deriving(Eq, Ord, Show)
 
 data Type = TVar String
+          | TInt
           | TArr Type Type
-          deriving(Eq, Ord)
+          deriving(Eq, Ord, Show)
 
 data Scheme = SForall String Scheme
             | SMono Type
-            deriving(Eq, Ord)
+            deriving(Eq, Ord, Show)
 
 type Context = Map String Scheme
 
@@ -28,7 +31,7 @@ data InProp = InJudgement Context Expr
             | InUnify Type Type
             | InInst Type Scheme
             | InIn String Scheme Context
-            deriving(Eq, Ord)
+            deriving(Eq, Ord, Show)
 
 -- | A proposition which is complete and requires no further computation (Judgement has type)
 data OutProp = OutJudgement Context Expr Type
@@ -36,7 +39,7 @@ data OutProp = OutJudgement Context Expr Type
              | OutUnify Type Type
              | OutInst Type Scheme
              | OutIn String Scheme Context
-             deriving(Eq, Ord)
+             deriving(Eq, Ord, Show)
 
 -- | a whole derivation/proof tree of an expression's type inference
 data InferenceTree =
@@ -44,6 +47,7 @@ data InferenceTree =
         OutProp -- | Root proposition (usually a judgment)
         (Maybe String) -- | name of typing rule if it's a judgment, Nothing otherwise
         [InferenceTree] -- | Required propositions
+    deriving(Eq, Ord, Show)
 
 -- | A proof tree with just one axiomatic proposition
 pureTree :: OutProp -> InferenceTree
@@ -52,19 +56,21 @@ pureTree prop = InferenceTree prop Nothing []
 instance Latex Expr where
     latexsPrec prec = \case
         Var x -> showString x
-        Lambda x body -> showParen (prec > 3) $ showString "\\lambda. " . showString x . latexsPrec 3 body
-        App f x -> showParen (prec > 2) $ latexsPrec 2 f . showString " " . latexsPrec 3 x
-        Let x rhs body -> showParen (prec > 3) $
-            showString "\textrm{let }"
+        Int n -> shows n
+        Lambda x body -> showParen (prec > 1) $ showString "\\lambda " . showString x . showString " . " . latexsPrec 3 body
+        App f x -> showParen (prec > 2) $ latexsPrec 2 f . showString "\\ " . latexsPrec 3 x
+        Let x rhs body -> showParen (prec > 1) $
+            showString "\\textrm{let }"
             . showString x
             . showString " = "
             . latexsPrec 0 rhs
-            . showString "\textrm{ in }"
+            . showString "\\textrm{ in }"
             . latexsPrec 0 body
 
 instance Latex Type where
     latexsPrec prec = \case
         TVar x -> showString x
+        TInt -> showString "nat"
         TArr arg ret -> showParen (prec > 1) $ latexsPrec 2 arg . showString " \\rightarrow " . latexsPrec 1 ret
 
 instance Latex Scheme where
@@ -84,7 +90,7 @@ instance Latex OutProp where
             latexContext ctx ++ "\\ \\vdash\\ " ++ latex e ++ " : " ++ latex t
         OutNewvar x -> x++" = newvar "
         OutUnify a b -> " unify("++latex a++" , "++latex b++")"
-        OutInst t s -> latex t++" = inst("++latex s++")"
+        OutInst t s -> latex t++" = instantiate("++latex s++")"
         OutIn x s ctx -> "("++x++" : "++latex s++") \\in "++latexContext ctx
     latexList xs = intercalateS "\\quad" (fmap latex xs)
 
@@ -93,8 +99,45 @@ instance Latex InferenceTree where
         InferenceTree prop Nothing _ -> latex prop
         InferenceTree prop (Just name) requirements ->
             "\\frac{\\displaystyle "++latex requirements++"}{\\displaystyle "++latex prop++"} "++name
+    latexList xs = intercalateS " \\quad " (fmap latex xs)
 
 freeMonoVars :: Type -> Set String
 freeMonoVars = \case
     TVar x -> Set.singleton x
+    TInt -> mempty
     TArr arg ret -> Set.union (freeMonoVars arg) (freeMonoVars ret)
+
+freeSchemeVars :: Scheme -> Set String
+freeSchemeVars = \case
+    SForall x s -> Set.delete x (freeSchemeVars s)
+    SMono t -> freeMonoVars t
+
+freeCtxVars :: Context -> Set String
+freeCtxVars ctx = mconcat (fmap freeSchemeVars (Map.elems ctx))
+
+subsMono :: Map String Type -> Type -> Type
+subsMono subs = \case
+    TVar x -> fromMaybe (TVar x) (Map.lookup x subs)
+    TInt -> TInt
+    TArr arg ret -> TArr (subsMono subs arg) (subsMono subs ret)
+
+subMono :: String -> Type -> Type -> Type
+subMono target replacement = \case
+    TVar x
+        | x == target -> replacement
+        | otherwise -> TVar x
+    TInt -> TInt
+    TArr arg ret -> TArr (subMono target replacement arg) (subMono target replacement ret)
+
+subsScheme :: Map String Type -> Scheme -> Scheme
+subsScheme subs = \case
+    SForall x s -> SForall x $ subsScheme subs' s
+        where subs' = Map.delete x subs
+    SMono t -> SMono $ subsMono subs t
+
+subScheme :: String -> Type -> Scheme -> Scheme
+subScheme target replacement = \case
+    SForall x s
+        | x == target -> SForall x s
+        | otherwise -> SForall x (subScheme target replacement s)
+    SMono t -> SMono $ subMono target replacement t
--- src/Typing.hs
+++ src/Typing.hs
@@ -13,34 +13,102 @@ import Data.Functor (($>))
 -- maybe error should just stop, but don't trash the tree bc you want to see
 -- where it errored in the tree
 
-type M a = State (UnionFind Type) a
+type M a = State (UnionFind Type, Integer) a
 
 unify :: Type -> Type -> M ()
 unify a b = do
-    uf <- get
+    (uf,n) <- get
     let a' = find uf a
         b' = find uf b
     case (a',b') of
         _ | a' == b' -> return ()
         (TArr arg ret, TArr arg' ret') ->
             zipWithM_ unify [arg,ret] [arg',ret']
+        (TInt, TInt) -> return ()
         (TVar x, t)
             | x `elem` freeMonoVars t -> undefined -- TODO
-            | otherwise -> put (union uf b' a')
+            | otherwise -> put (union uf b' a',n)
         (t, TVar x)
             | x `elem` freeMonoVars t -> undefined -- TODO
-            | otherwise -> put (union uf a' b')
+            | otherwise -> put (union uf a' b',n)
+        (TArr{},_) -> undefined -- TODO
+        (TInt,_) -> undefined -- TODO
 
+newvar :: M String
+newvar = do
+    (uf,n) <- get
+    put (uf,succ n)
+    return ("t"++show n)
 
+instantiate :: Scheme -> M Type
+instantiate = \case
+    SForall x s -> do
+        t <- TVar <$> newvar
+        let s' = subScheme x t s
+        instantiate s'
+    SMono t -> return t
 
+generalize :: Context -> Type -> Scheme
+generalize ctx t = s
+    where monoFrees = freeMonoVars t
+          ctxFrees = freeCtxVars ctx
+          frees = monoFrees `Set.difference` ctxFrees
+          s = foldr SForall (SMono t) frees
 
-step :: InProp -> M (OutProp, Maybe String, [InProp])
-step = \case
+-- | Assuming an inference tree is rooted with a typing judgement, get its inferred type.
+getTreeType :: InferenceTree -> Type
+getTreeType = \case
+    InferenceTree (OutJudgement _ _ t) _ _ -> t
+    _ -> error "expected a judgement"
+
+infer :: InProp -> M InferenceTree
+infer = \case
     InJudgement ctx e -> case e of
-        _ -> undefined
-    InNewvar x -> return (OutNewvar x, Nothing, [])
-    InUnify a b -> unify a b $> (OutUnify a b, Nothing, [])
-    InInst t s -> return (OutInst t s, Nothing, [])
+        Var x -> case Map.lookup x ctx of
+            Nothing -> undefined -- TODO
+            Just s -> do
+                t <- instantiate s
+                InferenceTree (OutJudgement ctx e t) (Just "Var") <$> mapM infer [InIn x s ctx, InInst t s]
+        Int{} -> return $ InferenceTree (OutJudgement ctx e TInt) (Just "Nat") []
+        Lambda x body -> do
+            x' <- newvar
+            let tX = TVar x'
+                ctx' = Map.insert x (SMono tX) ctx
+            bodyTree <- infer (InJudgement ctx' body)
+            let tBody = getTreeType bodyTree
+                tLambda = TArr tX tBody
+            nvTree <- infer (InNewvar x')
+            return $ InferenceTree (OutJudgement ctx e tLambda) (Just "Abs") [nvTree, bodyTree]
+        Let x rhs body -> do
+            rhsTree <- infer (InJudgement ctx rhs)
+            let tRhs = getTreeType rhsTree
+                sRhs = generalize ctx tRhs
+                ctx' = Map.insert x sRhs ctx
+            bodyTree <- infer (InJudgement ctx' body)
+            let tBody = getTreeType bodyTree
+            return $ InferenceTree (OutJudgement ctx e tBody) (Just "Let") [rhsTree, bodyTree]
+        App f x -> do
+            fTree <- infer (InJudgement ctx f)
+            xTree <- infer (InJudgement ctx x)
+            t_ <- newvar
+            nvTree <- infer (InNewvar t_)
+            let tRet = TVar t_
+                tF = getTreeType fTree
+                tX = getTreeType xTree
+            unifyTree <- infer (InUnify (TArr tX tRet) tF)
+            return $ InferenceTree (OutJudgement ctx e tRet) (Just "App") [fTree, xTree, nvTree, unifyTree]
+    InNewvar x -> return $ pureTree (OutNewvar x)
+    InUnify a b -> unify a b $> pureTree (OutUnify a b)
+    InInst t s -> return . pureTree $ OutInst t s
     InIn x s ctx
-        | Map.member x ctx -> return (OutIn x s ctx, Nothing, [])
+        | Map.member x ctx -> return . pureTree $ OutIn x s ctx
         | otherwise -> undefined -- TODO
+
+runInfer :: M a -> a
+runInfer m = evalState m (mempty,1)
+
+inferExpr :: Expr -> Type
+inferExpr e = getTreeType (runInfer (infer (InJudgement mempty e)))
+
+renderInference :: Expr -> String
+renderInference e = latex $ runInfer (infer (InJudgement mempty e))
